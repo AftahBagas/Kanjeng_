9992525ceb366232e0f37927d239cc9719b8a717
AftahBagas <77558501+AftahBagas@users.noreply.github.com>
2021-05-03 21:57:05 +0700

    Update events.py

--- userbot/events.py
+++ userbot/events.py
@@ -97,11 +97,11 @@ def register(**args):
                 if not disable_errors:
                     date = strftime("%Y-%m-%d %H:%M:%S", gmtime())
 
-                    text = "**Petercord User ERROR**\n"
-                    link = "Silahkan chat: @bismillahselaluadaa"
+                    text = "**Kanjeng User ERROR**\n"
+                    link = "Silahkan chat: @kanjengingsun"
                     text += "Untuk melaporkan kesalahan"
                     text += f"- tinggal teruskan pesan ini {link}.\n"
-                    text += "ILham Mansiezz Siap Membantu Kamu\n"
+                    text += "Alfareza Siap Membantu Kamu\n"
 
                     ftext = "========== DISCLAIMER =========="
                     ftext += "\nThis file uploaded ONLY here,"
