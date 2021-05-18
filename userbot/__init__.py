8b5efc4c485e7be7df4929e372a8c6e0d9a6ab20
AftahBagas <77558501+AftahBagas@users.noreply.github.com>
2021-05-18 14:33:26 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -167,7 +167,7 @@ ZIP_DOWNLOAD_DIRECTORY = os.environ.get("ZIP_DOWNLOAD_DIRECTORY", "./zips")
 BITLY_TOKEN = os.environ.get("BITLY_TOKEN", None)
 
 # Bot Name
-TERM_ALIAS = os.environ.get("TERM_ALIAS", "Kanjeng-Userbot")
+TERM_ALIAS = os.environ.get("TERM_ALIAS", "Kanjeng_")
 
 # Bot version
 BOT_VER = os.environ.get("BOT_VER", "4.0")
